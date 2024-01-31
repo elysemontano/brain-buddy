f51fe4cb0eb96f82e389bbe720958a12e8745fb6
Elyse Montano <elysemontano.dev.com>
2024-01-30 19:45:54 -0800

    adds controller actions, request specs with 90% coverage

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,37 @@
 class CardsController < ApplicationController
+  def index
+    cards = Card.all 
+    render json: cards
+  end
+
+  def create 
+    card = Card.create(card_params)
+    if card.valid? 
+      render json: card 
+    else 
+      render json: card.errors, status: 422
+    end
+  end
+
+  def update 
+    card = Card.find(params[:id])
+    card.update(card_params)
+    if card.valid?
+      render json: card 
+    else 
+      render json: card.errors, status: 422
+    end
+  end
+
+  def destroy 
+    card = Card.find(params[:id])
+    if card.destroy
+      render json: card
+    end
+  end
+
+  private 
+  def card_params
+    params.require(:card).permit(:user_id, :topic_id, :question, :answer)
+  end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,37 @@
 class TopicsController < ApplicationController
+  def index
+    topics = Topic.all 
+    render json: topics
+  end
+
+  def create 
+    topic = Topic.create(topic_params)
+    if topic.valid? 
+      render json: topic 
+    else
+      render json: topic.errors, status: 422
+    end
+  end
+
+  def update 
+    topic = Topic.find(params[:id])
+    topic.update(topic_params)
+    if topic.valid? 
+      render json: topic 
+    else
+      render json: topic.errors, status: 422
+    end
+  end
+
+  def destroy 
+    topic = Topic.find(params[:id])
+    if topic.destroy 
+      render json: topic
+    end
+  end
+
+  private
+  def topic_params 
+    params.require(:topic).permit(:name, :description, :user_id)
+  end
 end
