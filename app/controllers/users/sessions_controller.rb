441df81b6af0e71bd0d61ac23aa439440f1c9b05
Elyse Montano <elysemontano.dev.com>
2024-03-18 14:54:52 -0700

    modifies routes and controllers for user login patch

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,18 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
-  private
-  def respond_with(resource, _opts = {})
-    render json: resource
+
+  def create
+    user = User.find_for_database_authentication(email: params[:email])
+    if user && user.valid_password?(params[:password])
+      sign_in(user)
+      render json: { user: user }, status: :ok
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
   end
-  def respond_to_on_destroy
-    render json: { message: "Logged out." }
+
+  def destroy
+    sign_out(current_user) if current_user
+    render json: { message: 'Logged out successfully' }, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users,
+  path: '',
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+authenticated :user do
+  get '/users/current_user', to: 'users#current_user'
+end
   resources :cards
   resources :topics
   resources :apartments
-  devise_for :users,
-    path: '',
-    path_names: {
-      sign_in: 'login',
-      sign_out: 'logout',
-      registration: 'signup'
-    },
-    controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations'
-    }
 end
